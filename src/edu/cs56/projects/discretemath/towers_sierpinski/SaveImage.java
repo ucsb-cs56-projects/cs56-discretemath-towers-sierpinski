24d52a1320f1c79cde4986b70e94b0ab0e3dfcb8
Shanen Cross <shanencross@umail.ucsb.edu>
2013-05-17 15:29:45 -0700

    Completed class (ignores background colors except that of the specific component passed in as an argument, though), seems to work properly

--- src/edu/cs56/projects/discretemath/towers_sierpinski/SaveImage.java
+++ src/edu/cs56/projects/discretemath/towers_sierpinski/SaveImage.java
@@ -1,22 +1,7 @@
 package edu.cs56.projects.discretemath.towers_sierpinski;
 
-// What jaco-a imports for class which draws triangles
 import java.awt.*;
-import java.awt.geom.*; // May not need this?
-import java.util.*; // Or... this? Or anything below?
 import javax.swing.*;
-import java.awt.event.*;
-
-// What Conrad imports in lab5 for saving image
-/*import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
-import java.awt.Color;
-import java.io.File;
-import javax.imageio.ImageIO;
-import java.io.IOException;
-*/
-
-// Probably still need these
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
@@ -26,38 +11,100 @@ import java.io.IOException;
  * @author Shanen Cross
  * @version CS56, S13, lab04
  *
- * Contains method which allows you to save the image in either the .jpg or .pnh format.
-*/
+ * Contains methods which allows you to save an image from a JPanel or other Component
+ * in either the .jpg or .png format.
+ */
 
 public class SaveImage {
 
-    public void save(Component comp) {
-	Dimension size = comp.getSize();
-	BufferedImage bufImage = new BufferedImage(size.width, size.height,BufferedImage.TYPE_INT_RGB);
-	Graphics2D g = bufImage.createGraphics();
-	comp.paint(g);
-
-	try {
-	    ImageIO.write(bufImage, "png", new File("SierpinskiTriangle.png"));
-	} catch (IOException ex) {
-	    ex.printStackTrace();
+	// These public static methods are the ones which should be called by other classes' methods
+	// if they wish to save the component's image.
+	
+	/**
+	 * Saves as .png file
+	 * @param comp GUI Component, e.g. JFrame or JPanel
+	 */
+	public static void savePNG(Component comp) 
+	{
+		save(comp, "png");
 	}
-    }
+	/**
+	 * Saves as .jpg file
+	 * @param comp GUI Component, e.g. JFrame or JPanel
+	 */
+	public static void saveJPG(Component comp) // Saves as .jpg file
+	{
+		save(comp, "jpg");
+	}
 
-    // main for TESTING PURPOSES ONLY
-    /* public static void main(String[] args) {
+	/**
+	 * Saves image to file
+	 * @param comp GUI Component, e.g. JFrame or JPanel
+	 * @param filetype Name of filetype, intended to be either png or jpg
+	 */
+    private static void save(Component comp, String filetype) {
+    	JFrame temp_frame = null; // We won't need this unless comp is a JFrame
+    	
+    	// If comp *is* a JFrame, we want to get the dimensions from its ContentPane,
+    	// not from the frame itself, so we don't get the window header in our image...
+    	if (comp instanceof JFrame) {  
+    		temp_frame = (JFrame)comp;
+    		comp = temp_frame.getContentPane(); //...So we point comp to the JFrame's ContentPane
+    	}										// (keeping the original JFrame around as temp_frame)
+    	
+    	// Then we get the image dimensions,
+    	// make a buffered image with those dimensions
+    	// and a graphics object from that image.
+    	Dimension size = comp.getSize(); 
+    	BufferedImage bufImage = new BufferedImage(size.width, size.height,BufferedImage.TYPE_INT_RGB);
+    	Graphics2D g = bufImage.createGraphics();
+
+    	// But getContentPane() doesn't carry over information about the background color,
+    	// So we have to get that separately and draw it on
+    	// We will use the color of whatever component is passed in
+    	
+    	if (temp_frame != null){ // If the original comp was a JFrame,
+    		g.setColor(temp_frame.getBackground()); // we want the background color from the JFrame,
+    	}										    // not the content pane, so we get that from temp_frame
+    	else {
+    		g.setColor(comp.getBackground()); // Otherwise, we get it from whatever our comp is
+    	}
+    	g.fillRect(0, 0, size.width, size.height); // Then we draw the background on...
+    	comp.printAll(g); // ...and paint our comp itself onto the graphics object
+    	g.dispose(); // Then the graphics object is useless to us and disposed of
+    	try { // Now we write the buffered image to a file
+    	    ImageIO.write(bufImage, filetype, new File("SierpinskiTriangle."+filetype));
+    	} catch (IOException ex) {
+    	    ex.printStackTrace();
+    	}
+}
+    // The following methods are all hold-overs from my testing phase in case anyone else would want to test
+    // these out similarly.
+    
+    // Called by main() for testing purposes
+    public void go() {
 	JFrame frame = new JFrame();
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	frame.getContentPane().add(BorderLayout.CENTER, new DrawPane());
-	frame.setSize(300,300);
+	DrawPane pane = new DrawPane();
+	frame.getContentPane().add(BorderLayout.CENTER, pane);
+	frame.setSize(300,400);
 	frame.setVisible(true);
-    }
+	frame.setBackground(new Color(0,200,0));
+	saveJPG(frame.getContentPane());
+	savePNG(frame);
+   }
 
-    public static class DrawPane extends JPanel{
-        public void paintComponent(Graphics g){
-	    //draw on g here e.g.
-	    g.fillRect(20, 20, 100, 200);
-	}
-	}*/
+    // For testing purposes, used in go()
+    public class DrawPane extends JPanel{
+    	public void paintComponent(Graphics g){
+     		g.setColor(new Color(200,20,10));
+    		g.fillRect(20, 20, 100, 200);
+    	}
+    }
+    
+    // For testing purposes
+    public static void main(String[] args) {
+    	SaveImage exp = new SaveImage();
+    	exp.go();
+    }
 }
