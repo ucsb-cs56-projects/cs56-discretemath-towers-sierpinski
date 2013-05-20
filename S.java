064195d745bef5ce67c2028db24a6ad59ed0fa5f
Gordon Cheung <gordoncheung@umail.ucsb.edu>
2013-05-19 20:20:26 -0700

    All disks centered properly, scaling text sizes, :)

--- S.java
+++ S.java
@@ -3,6 +3,7 @@ import javax.swing.JScrollPane;
 
 import java.awt.BasicStroke;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
@@ -11,6 +12,7 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 
@@ -57,8 +59,14 @@ public class S extends JPanel{
 	}
 	
 	Graphics2D g2d = (Graphics2D) g;
-	Font font = new Font("Arial", Font.BOLD, 14);
-	g2d.setFont(font);
+	if(lefttopright.length()==0){
+	    Font font = new Font("Arial", Font.BOLD, 14);
+	    g2d.setFont(font);
+	}
+	else{
+	    Font font = new Font("Arial", Font.BOLD,25/lefttopright.length());
+	    g2d.setFont(font);
+	}
 	
 	double x = sideLength * Math.cos(Math.PI/3);
 	double y = sideLength * Math.sin(Math.PI/3);
@@ -89,9 +97,9 @@ public class S extends JPanel{
 	  else if(lefttopright.charAt(r)!='T'){
 	  System.out.println("everytime= "+lefttopright);
 	  //break;
-	  }
+    }
 	  
-	  }
+    }
 	*/
 	/* save for lefttopright.length() == 1
 	   if(lefttopright.charAt(q)=='L'){
@@ -100,225 +108,227 @@ public class S extends JPanel{
 	   tcX -= 1.5*x;
 	   }
 	   else if(lefttopright.charAt(q)=='R'){
-	rcX += 1.5*x;
-	tcX += 1.5*x;
-    }
-	
+	   rcX += 1.5*x;
+	   tcX += 1.5*x;
+	   }
+	   
 	   else if(lefttopright.charAt(q)=='T' && q==0){
-	lcX -= .75*x;
-	rcX += .75*x;
-    }
+	   lcX -= .75*x;
+	   rcX += .75*x;
+	   }
 	   else if(lefttopright.charAt(q)=='T' && q!=0){
-	lcX -= .75*x;
-	rcX += .75*x;
-	
-	if(lefttopright.charAt(q-1)=='L'){
-	lcX -=1.5*x;
-    }
-	else if(lefttopright.charAt(q-1)=='R'){
-	rcX +=1.5*x;
-    }
-	
-    }
+	   lcX -= .75*x;
+	   rcX += .75*x;
+	   
+	   if(lefttopright.charAt(q-1)=='L'){
+	   lcX -=1.5*x;
+	   }
+	   else if(lefttopright.charAt(q-1)=='R'){
+	   rcX +=1.5*x;
+	   }
+	   
+	   }
 	*/
-	if(lefttopright.length()==0{
-	    }
-	    else{	
-	block:{
-	    
-	    //for(int q = 0; q<lefttopright.length();q++){
-	    
-	    if(lefttopright.charAt(0)=='T'){
-		if(lefttopright.charAt(lefttopright.length()-1)=='L'){
-		    lcX -= 1.5*x;
-		    rcY += y/2;
-		    tcX -= 1.5*x;
-		    boolean isR=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('R' == lefttopright.charAt(q)) isR=true;
-		    }
-		    if(isR){
-			lcX+=1.5*x;
-			lcY+=y/2;
-		    }
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
-		    rcX += 1.5*x;
-		    lcY += y/2;
-		    tcX += 1.5*x;
-		    boolean isL=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('L' == lefttopright.charAt(q)) isL=true;
-		    }
-		    if(isL){
-			rcX-=1.5*x;
-			rcY+=y/2;
-		    }
-		    
-		    
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='T'){
-		    rcX += 1.5*x;
-		    lcX -= 1.5*x;
-		    boolean allSame=true;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
-		    }
-		    if(allSame){
-		    }
-		    else{
-			boolean noL=true;
-			boolean noR=true;
-			int indexL=0;
-			int indexR=0;
+	
+	if(lefttopright.length()==0){
+	    System.out.println("hihi");
+	}
+	else{
+	    block:{
+		
+		//for(int q = 0; q<lefttopright.length();q++){
+		
+		if(lefttopright.charAt(0)=='T'){
+		    if(lefttopright.charAt(lefttopright.length()-1)=='L'){
+			lcX -= 1.5*x;
+			rcY += y/2;
+			tcX -= 1.5*x;
+			boolean isR=false;
 			for(int q=0; q<lefttopright.length()-1; q++){
-			    if('L' == lefttopright.charAt(q)) {noL=false; indexL=q;}
-			    if('R' == lefttopright.charAt(q)) {noR=false; indexR=q;}
-			    
+			    if('R' == lefttopright.charAt(q)) isR=true;
 			}
-			if(!noL && !noR){
-			    if(indexL>indexR)
-				tcX -=1.5*x;
-			    else
-				tcX+=1.5*x;
+			if(isR){
+			    lcX+=1.5*x;
+			    lcY+=y/2;
 			}
-			else if(noL){
-			    tcX += 1.5*x;
+		    }
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
+			rcX += 1.5*x;
+			lcY += y/2;
+			tcX += 1.5*x;
+			boolean isL=false;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if('L' == lefttopright.charAt(q)) isL=true;
+			}
+			if(isL){
+			    rcX-=1.5*x;
+			    rcY+=y/2;
 			}
 			
-			else if(!noL){
-			    tcX -= 1.5*x;
-			}
 			
 		    }
-		}
-	    }
-	    else if(lefttopright.charAt(0)=='R'){
-		if(lefttopright.charAt(lefttopright.length()-1)=='T'){
-		    rcX += 1.5*x;
-		    lcX -= 1.5*x;
-		    tcX += 1.5*x;
-		    boolean isL=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('L' == lefttopright.charAt(q)) isL=true;
-		    }
-		    if(isL){
-			tcX-=3*x;
-		    }
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='L'){
-		    tcX -=1.2*x;
-		    lcY += y/2;
-		    rcY += y/2;
-		    boolean isT=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('T' == lefttopright.charAt(q)) isT=true;
-		    }
-		    if(isT){
-			lcY-=y/2;
-			lcX-=1.5*x;
-		    }
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
-		    tcX += 1.5*x;
-		    lcY += y/2;
-		    
-		    boolean allSame=true;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
-		    }
-		    if(allSame){
-			rcX +=1.5*x;
-		    }
-		    else{ 
-			boolean noL=true;
-			boolean noT=true;
-			int indexL=0;
-			int indexT=0;
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='T'){
+			rcX += 1.5*x;
+			lcX -= 1.5*x;
+			boolean allSame=true;
 			for(int q=0; q<lefttopright.length()-1; q++){
-			    if('L' == lefttopright.charAt(q)) {noL=false; indexL=q;}
-			    if('T' == lefttopright.charAt(q)) {noT=false; indexT=q;}
+			    if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
 			}
-			if(!noL && !noT){
-			    if(indexL < indexT)
-				rcX += 1.5*x;
-			    else
-				rcY += y/2;
-			    //rcY -= y/2;
-			}
-			else if(noL){
-			    rcX += 1.5*x;
+			if(allSame){
 			}
 			else{
-			    rcY += y/2;
+			    boolean noL=true;
+			    boolean noR=true;
+			    int indexL=0;
+			    int indexR=0;
+			    for(int q=0; q<lefttopright.length()-1; q++){
+				if('L' == lefttopright.charAt(q)) {noL=false; indexL=q;}
+				if('R' == lefttopright.charAt(q)) {noR=false; indexR=q;}
+				
+			    }
+			    if(!noL && !noR){
+				if(indexL>indexR)
+				    tcX -=1.5*x;
+				else
+				    tcX+=1.5*x;
+			    }
+			    else if(noL){
+				tcX += 1.5*x;
+			    }
+			    
+			    else if(!noL){
+				tcX -= 1.5*x;
+			    }
+			    
 			}
 		    }
 		}
-	    }
-	    else if(lefttopright.charAt(0)=='L'){
-		if(lefttopright.charAt(lefttopright.length()-1)=='T'){
-		    rcX += 1.5*x;
-		    lcX -= 1.5*x;
-		    tcX -= 1.5*x;
-		    boolean isR=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('R' == lefttopright.charAt(q)) isR=true;
-		    }
-		    if(isR){
-			tcX+=3*x;
-		    }
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
-		    tcX += 1.2*x;
-		    lcY += y/2;
-		    rcY += y/2;
-		    boolean isT=false;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if('T' == lefttopright.charAt(q)) isT=true;
-		    }
-		    if(isT){
-			rcY-=y/2;
-			rcX+=1.5*x;
-		    }
-		}
-		else if(lefttopright.charAt(lefttopright.length()-1)=='L'){
-		    tcX -= 1.5*x;
-		    rcY += y/2;
-		    boolean allSame=true;
-		    for(int q=0; q<lefttopright.length()-1; q++){
-			if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
-		    }
-		    if(allSame){
+		else if(lefttopright.charAt(0)=='R'){
+		    if(lefttopright.charAt(lefttopright.length()-1)=='T'){
+			rcX += 1.5*x;
 			lcX -= 1.5*x;
-		    }
-		    else{
-			boolean noL=true;
-			boolean noT=true;
-			int indexR=0;
-			int indexT=0;
+			tcX += 1.5*x;
+			boolean isL=false;
 			for(int q=0; q<lefttopright.length()-1; q++){
-			    if('R' == lefttopright.charAt(q)) {noL=false; indexR=q;}
-			    if('T' == lefttopright.charAt(q)) {noT=false; indexT=q;}
+			    if('L' == lefttopright.charAt(q)) isL=true;
 			}
-			if(!noL && !noT){
-			    if(indexR < indexT)
-				lcX -= 1.5*x;
-			    else
-				lcY += y/2;
+			if(isL){
+			    tcX-=3*x;
 			}
-			else if(noL){
+		    }
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='L'){
+			tcX -=1.2*x;
+			lcY += y/2;
+			rcY += y/2;
+			boolean isT=false;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if('T' == lefttopright.charAt(q)) isT=true;
+			}
+			if(isT){
+			    lcY-=y/2;
+			    lcX-=1.5*x;
+			}
+		    }
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
+			tcX += 1.5*x;
+			lcY += y/2;
+			
+			boolean allSame=true;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
+			}
+			if(allSame){
+			    rcX +=1.5*x;
+			}
+			else{ 
+			    boolean noL=true;
+			    boolean noT=true;
+			    int indexL=0;
+			    int indexT=0;
+			    for(int q=0; q<lefttopright.length()-1; q++){
+				if('L' == lefttopright.charAt(q)) {noL=false; indexL=q;}
+				if('T' == lefttopright.charAt(q)) {noT=false; indexT=q;}
+			    }
+			    if(!noL && !noT){
+				if(indexL < indexT)
+				    rcX += 1.5*x;
+				else
+				    rcY += y/2;
+				//rcY -= y/2;
+			    }
+			    else if(noL){
+				rcX += 1.5*x;
+			    }
+			    else{
+				rcY += y/2;
+			    }
+			}
+		    }
+		}
+		else if(lefttopright.charAt(0)=='L'){
+		    if(lefttopright.charAt(lefttopright.length()-1)=='T'){
+			rcX += 1.5*x;
+			lcX -= 1.5*x;
+			tcX -= 1.5*x;
+			boolean isR=false;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if('R' == lefttopright.charAt(q)) isR=true;
+			}
+			if(isR){
+			    tcX+=3*x;
+			}
+		    }
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='R'){
+			tcX += 1.2*x;
+			lcY += y/2;
+			rcY += y/2;
+			boolean isT=false;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if('T' == lefttopright.charAt(q)) isT=true;
+			}
+			if(isT){
+			    rcY-=y/2;
+			    rcX+=1.5*x;
+			}
+		    }
+		    else if(lefttopright.charAt(lefttopright.length()-1)=='L'){
+			tcX -= 1.5*x;
+			rcY += y/2;
+			boolean allSame=true;
+			for(int q=0; q<lefttopright.length()-1; q++){
+			    if(lefttopright.charAt(0) != lefttopright.charAt(q)) allSame=false;
+			}
+			if(allSame){
 			    lcX -= 1.5*x;
 			}
 			else{
-			    lcY += y/2;
+			    boolean noL=true;
+			    boolean noT=true;
+			    int indexR=0;
+			    int indexT=0;
+			    for(int q=0; q<lefttopright.length()-1; q++){
+				if('R' == lefttopright.charAt(q)) {noL=false; indexR=q;}
+				if('T' == lefttopright.charAt(q)) {noT=false; indexT=q;}
+			    }
+			    if(!noL && !noT){
+				if(indexR < indexT)
+				    lcX -= 1.5*x;
+				else
+				    lcY += y/2;
+			    }
+			    else if(noL){
+				lcX -= 1.5*x;
+			    }
+			    else{
+				lcY += y/2;
+			    }
 			}
 		    }
 		}
+		
 	    }
 	    
 	}
-	
-	    }
 	
 	
 	drawTowers(tcX,tcY,sideLength,g);
@@ -327,25 +337,42 @@ public class S extends JPanel{
 	
 	Point2D.Double[] p = {new Point2D.Double(lcX,lcY),new Point2D.Double(tcX,tcY),new Point2D.Double(rcX,rcY)};
 
-	
+	int lastDiskWidth=0;
 	int[] disks = {1,1,1};
 	String hBase = "0,1,2";
 	for (int i=0;i<lefttopright.length();i++){
-	    double newWidth= tW*(lefttopright.length()-i*.5)/lefttopright.length();
-	    if(lefttopright.length()==0)
+	    lastDiskWidth=i;
+	    double newWidth=tW;
+	    if(lefttopright.length()==0){
 		newWidth=tW;
+	    }
+	    else{
+		newWidth= tW*(lefttopright.length()-i*.5)/lefttopright.length();
+	    }
 	    String[] h = hBase.split(",");
 	    switch(lefttopright.charAt(i)){
 	    case 'T': 
 		if (depth==0){
 		    int d = Integer.parseInt(h[1]);
 		    for (int j = 0; j < 3; j++){
-			g2d.setColor(colors.get(n-i));
-			Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/6, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
-			g2d.fill(disk2);
-			//g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d],newWidth/3,eH));
-			g2d.setColor(Color.BLACK);
-			g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3),(float)(p[j].y-eH*(disks[d]-1)));
+			if(lefttopright.length()==0){
+			    
+			    g2d.setColor(colors.get(n-i));
+			    
+			    Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/6, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
+			    g2d.fill(disk2);
+			    
+			    //g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d],newWidth/3,eH));
+			    g2d.setColor(Color.BLACK);
+			    g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3),(float)(p[j].y-eH*(disks[d]-1)));
+			}
+			else{
+			    g2d.setColor(colors.get(n-i));
+			    g2d.fill(new Ellipse2D.Double(p[j].x-newWidth/2+d*newWidth/3, p[j].y-eH*disks[d],newWidth/3,eH));
+			    g2d.setColor(Color.BLACK);
+			    g2d.drawString(""+(n-i),(float)(p[j].x-newWidth/3+d*newWidth/3),(float)(p[j].y-eH*(disks[d]-1)));
+			    
+			}
 		    }
 		    disks[d]++;
 		    hBase = ""+h[2]+","+h[1]+","+h[0];
@@ -357,13 +384,27 @@ public class S extends JPanel{
 		    int d = Integer.parseInt(h[2]);
 		    for (int j = 0; j < 3; j++){
 			g2d.setColor(colors.get(n-i));
+			if(i==0){
+			    Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/12, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
+			    g2d.fill(disk2);
+			    g2d.setColor(Color.BLACK);
+
+			    g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3-tW/12),(float)(p[j].y-eH*(disks[d]-1)));
+
+			}
 			
-			Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/6, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
-			g2d.fill(disk2);
+			
+			else{
+			    g2d.setColor(colors.get(n-i));
+			    g2d.fill(new Ellipse2D.Double(p[j].x-newWidth/2+d*newWidth/3, p[j].y-eH*disks[d],newWidth/3,eH));
+			    g2d.setColor(Color.BLACK);
+			    g2d.drawString(""+(n-i),(float)(p[j].x-newWidth/3+d*newWidth/3),(float)(p[j].y-eH*(disks[d]-1)));
+			    
+			}
+
 			//g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d],newWidth/3,eH));
-			g2d.setColor(Color.BLACK);
-			g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3-tW/12),(float)(p[j].y-eH*(disks[d]-1)));
 		    }
+		    
 		    disks[d]++;
 		    hBase= ""+h[1]+","+h[0]+","+h[2];
 		}
@@ -372,12 +413,20 @@ public class S extends JPanel{
 		if(depth==0){
 		    int d = Integer.parseInt(h[0]);
 		    for (int j = 0; j < 3; j++){
+			
 			g2d.setColor(colors.get(n-i));
-			Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/8, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
-			g2d.fill(disk2);
-			//g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d],newWidth/3,eH));
-			g2d.setColor(Color.BLACK);
-			g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3-tW/12),(float)(p[j].y-eH*(disks[d]-1)));
+			if(i==0){
+			    Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/4, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
+			    g2d.fill(disk2);
+			    g2d.setColor(Color.BLACK);
+			    g2d.drawString(""+(n-i),(float)(p[j].x-tW/3+d*tW/3+tW/12),(float)(p[j].y-eH*(disks[d]-1)));
+			}
+			else{
+			    g2d.setColor(colors.get(n-i));
+			    g2d.fill(new Ellipse2D.Double(p[j].x-newWidth/2+d*newWidth/3, p[j].y-eH*disks[d],newWidth/3,eH));
+			    g2d.setColor(Color.BLACK);
+			    g2d.drawString(""+(n-i),(float)(p[j].x-newWidth/3+d*newWidth/3),(float)(p[j].y-eH*(disks[d]-1)));
+			}
 		    }
 		    disks[d]++;
 		    hBase=""+h[0]+","+h[2]+","+h[1];
@@ -387,8 +436,8 @@ public class S extends JPanel{
 
 	}
 	
-	if (depth==0){	    
-	    double newWidth= tW/lefttopright.length();
+	if (depth==0){
+	    double newWidth= tW*(lefttopright.length()-(lastDiskWidth+1)*.5)/lefttopright.length();
 	    if(lefttopright.length()==0){
 		newWidth=tW;
 		String[] h = hBase.split(",");
@@ -406,27 +455,37 @@ public class S extends JPanel{
 		}
 	    }
 	    else{
-		if(lefttopright.length()==1)newWidth=tW/2;
-		String[] h = hBase.split(",");
-		for (int j = 0; j < 3; j++){
-		    int d = Integer.parseInt(h[j]);
-		    g2d.setColor(colors.get(0));
-		    Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3+tW/6, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
-		    g2d.fill(disk2);
-		    //g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3+tW/12, p[j].y-eH*disks[d],newWidth/3,eH));
-		    g2d.setColor(Color.BLACK);
-		    if(lefttopright.length()==0)
-			g2d.drawString(""+0,(float)(p[j].x-tW/3+d*tW/3),(float)(p[j].y-eH*(disks[d]-1)));
-		    else
-			g2d.drawString(""+0,(float)(p[j].x-tW/3+d*tW/3-tW/16),(float)(p[j].y-eH*(disks[d]-1)));
+		if (depth==0){
+		    String[] h = hBase.split(",");
+		    g2d.setFont(eF);
+		    for (int j = 0; j < 3; j++){
+			int d = Integer.parseInt(h[j]);
+			//if(p[j].x == p[1].x)System.out.println("ever");
+			//if(p[j].x > tcX){
+			g2d.setColor(colors.get(0));
+			
+			//Ellipse2D disk2 = centerOfDisk(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
+			Ellipse2D disk2 = centerOfDisk(p[j].x, p[j].y-eH*disks[d]+eH/2,newWidth/3,eH);
+
+			g2d.fill(new Ellipse2D.Double(p[j].x-newWidth/3/2-newWidth/2+d*newWidth/2, p[j].y-eH*disks[d],newWidth/3,eH));
+			if(lefttopright.length()==0){
+			    Font font = new Font("Arial", Font.BOLD, 14);
+			    g2d.setFont(font);
+			}
+			else{
+			    Font font = new Font("Arial", Font.BOLD,25/lefttopright.length());
+			    g2d.setFont(font);
+			}
+			
+			//g2d.fill(new Ellipse2D.Double(p[j].x-tW/2+d*tW/3, p[j].y-eH*disks[d],newWidth/3,eH));
+			g2d.setColor(Color.BLACK);
+			g2d.drawString(""+0,(float)(p[j].x-newWidth/2-newWidth/2+newWidth/2+d*newWidth/2),(float)(p[j].y-eH*(disks[d]-1)));
+			
+		    }
 		}
+		
 	    }
 	}
-	
-	//placeDisk(set1,g, tower1coordx,tower2coordx,tower3coordx,towercoordy,depth,sideLength);
-	//placeDisk(set2,g, xtower11,xtower21,xtower31,ytower,depth,sideLength);
-	//placeDisk(set3,g, xtower12,xtower22,xtower32,ytower,depth,sideLength);
-	
 	
     }
     
@@ -503,7 +562,20 @@ public class S extends JPanel{
 
 	//draw Strings along lines
 	AffineTransform at = new AffineTransform();
-	g2.setFont(mF);
+	
+	
+	if(order.length()==0){
+	    //mF = new Font("Arial", Font.BOLD, (int) (14));
+	    g2.setFont(mF);
+	}
+	else{
+	    mF = new Font("Arial", Font.BOLD,(int) Math.sqrt(sL));
+	    g2.setFont(mF);
+	    System.out.println("SL is: "+sL);
+	}
+	
+	
+	
 	g2.drawString(move+b[1].charAt(0)+"->"+b[1].charAt(1),(float)(bX+tS),(float)(bY+tS)+(float)sL/15);
 	at.rotate(-Math.PI/3);
 	g2.setFont(mF.deriveFont(at));
@@ -511,7 +583,8 @@ public class S extends JPanel{
 	at.rotate(2*Math.PI/3);
 	g2.setFont(mF.deriveFont(AffineTransform.getRotateInstance(Math.PI/3)));
 	g2.drawString(move+b[2].charAt(0)+"->"+b[2].charAt(1),(float)(tX+x1+tS),(float)(tY+y1+tS));
-
+	
+	
 	drawSystem(depth,colors,order,g,tX,tY,sL);
     }
 
@@ -539,9 +612,8 @@ public class S extends JPanel{
     public static void main(String[] args){
 	System.out.println("start");
 	JFrame f = new JFrame("yo");
-	//f.setContentPane(new Tester(Integer.parseInt(args[0])));
-	//S s = new S(4);
-	S s = new S(Integer.parseInt(args[0]));
+	f.setContentPane(new Tester(Integer.parseInt(args[0])));
+	//S s = new S(6);
 	JScrollPane jsp = new JScrollPane(s,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 	f.add(jsp);
 	//f.setContentPane(new S(4));
