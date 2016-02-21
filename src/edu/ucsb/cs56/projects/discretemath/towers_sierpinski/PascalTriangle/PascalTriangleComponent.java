85649e601a160961317421d12b9c0a2d47c2a949
Caitlin Scarberry <caitlinscarberry@umail.ucsb.edu>
2016-02-20 20:11:00 -0800

    fixes issue #40, adds ability to change number of rows in pascal's triangle

--- src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleComponent.java
+++ src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleComponent.java
@@ -11,8 +11,9 @@ import java.awt.Color; // class for Colors
 
 /**
    The component that actually draws Pascal's Triangle. Everything is decently scalable besides the numbers in Pascal's Triangle which are hard coded. Just remember that there must be the same number of squares as numbers in Pascal's Triangle.
-   @author George Shih 
-   @version CS56 Winter 2014
+   @author George Shih
+   @author Caitlin Scarberry
+   @version CS56 Winter 2016
 */
 
 public class PascalTriangleComponent extends JComponent {  
@@ -25,38 +26,70 @@ public class PascalTriangleComponent extends JComponent {
     private int count = 0; // Total number of squares
     private ArrayList<Rectangle> squares = new ArrayList<Rectangle>();
 
-    private int [] numbersInPascalTriangle = {1,1,1,1,2,1,1,3,3,1,1,4,6,4,1,1,5,10,10,5,1,1,6,15,20,15,6,1,1,7,21,35,35,21,7,1}; // HARD CODED, make sure to have the same number of elements as squares or you'll get an Array Index Out Of Bounds Exception
+    /*important note: because the max integer size is 2,147,483,647, any
+      triangle with more than 33 rows will not be calculated correctly*/
+    private int [] numbersInPascalTriangle;
 
+    public PascalTriangleComponent(int rows){
+    	totalRows = rows;
+    	numbersInPascalTriangle = new int[rows*(rows+1)/2];
+    	numbersInPascalTriangle[0]=1;
+    	numbersInPascalTriangle[1]=1;
+    	numbersInPascalTriangle[2]=1;
+    	int currentRow = 2;
+    	int currentColumn = 0;
+    	for(int i = 3; i < numbersInPascalTriangle.length; i++){
+			int startPrevRow = ((currentRow-1)*(currentRow))/2;
+			int leftParent = currentColumn>0? numbersInPascalTriangle[startPrevRow + currentColumn-1]:0;
+			int rightParent = currentColumn<(currentRow) ? numbersInPascalTriangle[startPrevRow + currentColumn]:0;
+			numbersInPascalTriangle[i] = leftParent+rightParent;
+
+    		currentColumn++;
+    		if(currentColumn>currentRow){
+    			currentColumn = 0;
+    			currentRow++;
+    		}
+    	}
+    }
 
     // Paint Method
     public void paintComponent(Graphics g) {  
+		Graphics2D g2 = (Graphics2D) g;
 
-	Graphics2D g2 = (Graphics2D) g;
-	g2.setFont(new Font("default", Font.BOLD, 24)); // Text is bold and size 24
+		//Scale the squares so that the triangle takes up the panel
+		spacing = (this.getHeight()-centerY-50)/(this.totalRows*6);
 
-	// Adds Rectangles into ArrayList
-	for (int i = 0; i < totalRows; i++) { // i = rows
-	    for (int j = 0; j <= i; j++) { // j = index within row
-		squares.add(new Rectangle(centerX + 6*j*spacing - 3*i*spacing, centerY + 6*i*spacing, 6*spacing, 6*spacing)); // clever formula for aligning squares ;)
-	        count++; 
-	    }
-	}
+		// Adds Rectangles into ArrayList
+		squares = new ArrayList<Rectangle>();
+		count = 0;
+		for (int i = 0; i < totalRows; i++) { // i = rows
+		    for (int j = 0; j <= i; j++) { // j = index within row
+			squares.add(new Rectangle(centerX + 6*j*spacing - 3*i*spacing, centerY + 6*i*spacing, 6*spacing, 6*spacing)); // clever formula for aligning squares ;)
+		        count++; 
+		    }
+		}
 
-	// Draws the squares and numbers and fills them in appropriately
-	for (int i = 0; i < count; i++) {
-	    g2.setColor(Color.RED); 
-	    g2.draw(squares.get(i));
+		// Draws the squares and numbers and fills them in appropriately
+		for (int i = 0; i < count; i++) {
+			//System.out.println("adding square");
+		    g2.setColor(Color.RED); 
+		    g2.draw(squares.get(i));
 
-	    g2.setColor(Color.BLACK); 
-	    if (numbersInPascalTriangle[i] % 2 == 1) // Fill in square only if ODD
-		g2.fillRect((int)squares.get(i).getX()+1, (int)squares.get(i).getY()+1, (int)squares.get(i).getWidth()-1, (int)squares.get(i).getHeight()-1); // The adding and subtracting of 1 is to have a "border" show up
+		    g2.setColor(Color.BLACK); 
+		    if (numbersInPascalTriangle[i] % 2 == 1) // Fill in square only if ODD
+			g2.fillRect((int)squares.get(i).getX()+1, (int)squares.get(i).getY()+1, (int)squares.get(i).getWidth()-1, (int)squares.get(i).getHeight()-1); // The adding and subtracting of 1 is to have a "border" show up
 
-	    g2.setColor(new Color(0,50,255)); // Blue
-	    if (numbersInPascalTriangle[i] < 10) // Single digit numbers alignment
-		g2.drawString(Integer.toString(numbersInPascalTriangle[i]), (int)squares.get(i).getX() + 2*spacing + 2, (int)squares.get(i).getY() + 4*spacing - 1); // The constants just fine-tune alignment
-	    if (numbersInPascalTriangle[i] >= 10) // Double digit numbers alignment
-		g2.drawString(Integer.toString(numbersInPascalTriangle[i]), (int)squares.get(i).getX() + 1*spacing + 2 , (int)squares.get(i).getY() + 4*spacing - 1); // The constants just fine-tune alignment 
-	}
+		    g2.setColor(new Color(0,50,255)); // Blue
+		    
+		    String num = ""+numbersInPascalTriangle[i];
+		    //make font scale with number length
+		    g2.setFont(new Font("default", Font.BOLD, (int)(spacing*(3.1-.3*num.length()) ) ));
+
+		    if (numbersInPascalTriangle[i] < 10) // Single digit numbers alignment
+			g2.drawString(Integer.toString(numbersInPascalTriangle[i]), (int)squares.get(i).getX() + 2*spacing + 2, (int)squares.get(i).getY() + 4*spacing - 1); // The constants just fine-tune alignment
+		    if (numbersInPascalTriangle[i] >= 10) // Double digit numbers alignment
+			g2.drawString(Integer.toString(numbersInPascalTriangle[i]), (int)squares.get(i).getX() + 1*spacing + 2 , (int)squares.get(i).getY() + 4*spacing - 1); // The constants just fine-tune alignment 
+		}
 
     } // End Paint Method
 
--- src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleGUI.java
+++ src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleGUI.java
@@ -3,21 +3,34 @@ package edu.ucsb.cs56.projects.discretemath.towers_sierpinski.PascalTriangle;
 import javax.swing.JFrame;
 
 /**
-   Runs a GUI which shows Pascal's Triangle and it's relations with Sierpinski's Triangle and the Tower of Hanoi.
-   @author George Shih
-   @version CS56 Winter 2014
+ *  Runs a GUI which shows Pascal's Triangle and it's relations with Sierpinski's Triangle and the Tower of Hanoi.
+ *  @author George Shih
+ *	@author	Caitlin Scarberry
+ *  @version CS56 Winter 2016
  */
 
 public class PascalTriangleGUI extends JFrame {
 
     public static void main(String[] args) {
-	JFrame frame = new JFrame();
-	frame.setSize(800,600);
-	frame.setTitle("Pascal's Triangle GUI");
-	frame.setVisible(true);
-	PascalTriangleComponent component = new PascalTriangleComponent();
-	frame.add(component);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	int rows = 8;
+    	if(args.length>0)
+    		try{
+    			rows = Integer.parseInt(args[0]);
+    			if(rows<=0)
+    				throw new NumberFormatException();
+    		}
+    		catch(NumberFormatException e){
+    			System.err.println("Please input a valid number of rows (an integer >= 1)");
+    			System.exit(0);
+    		}
+
+		JFrame frame = new JFrame();
+		frame.setSize(800,600);
+		frame.setTitle("Pascal's Triangle GUI");
+		PascalTriangleComponent component = new PascalTriangleComponent(rows);
+		frame.add(component);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
     }
 }
 
