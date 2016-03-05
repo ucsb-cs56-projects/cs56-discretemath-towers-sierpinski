fc81aa247679c1197f2044c9b2b699555db89e4b
Sierra Schwellenbach <sschwellenbach@umail.ucsb.edu>
2016-03-05 15:19:03 -0800

    fixed issue #45 ~estimated 250 points

--- src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleComponent.java
+++ src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleComponent.java
@@ -17,6 +17,7 @@ import java.awt.Color; // class for Colors
    The component that actually draws Pascal's Triangle. Everything is decently scalable besides the numbers in Pascal's Triangle which are hard coded. Just remember that there must be the same number of squares as numbers in Pascal's Triangle.
    @author George Shih
    @author Caitlin Scarberry
+   @author Sierra Schwellenbach
    @version CS56 Winter 2016
 */
 
@@ -62,65 +63,66 @@ public class PascalTriangleComponent extends JComponent {
 
     // Paint Method
     public void paintComponent(Graphics g) {  
+		
+	       
 		Graphics2D g2 = (Graphics2D) g;
-
-        //Center everything
-        int centerX = this.getWidth()/2;
+		//Center everything
+		int centerX = this.getWidth()/2;
 
 
 		//Scale the hexagons so that the triangle takes up the panel
 		hexRadius = (int)((this.getHeight()-centerY-50)/(this.totalRows*1.5));
-        hexRadius = Math.min(hexRadius, (this.getWidth()-100)/(this.totalRows*2));
+		hexRadius = Math.min(hexRadius, (this.getWidth()-100)/(this.totalRows*2));
 
-        //make the base hexagon shape
-        int xPoints[] = {0, hexRadius,  hexRadius*2,  hexRadius*2, hexRadius, 0,0};
-        int yPoints[] = {0, -hexRadius/2, 0, hexRadius, 3*hexRadius/2, hexRadius,0};
-        GeneralPath hexagon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, xPoints.length);
-        hexagon.moveTo(xPoints[0], yPoints[0]);
-        for (int index = 1; index < xPoints.length; index++) {
-                hexagon.lineTo(xPoints[index], yPoints[index]);
-        };
-        hexagon.closePath();
+		//make the base hexagon shape
+		int xPoints[] = {0, hexRadius,  hexRadius*2,  hexRadius*2, hexRadius, 0,0};
+		int yPoints[] = {0, -hexRadius/2, 0, hexRadius, 3*hexRadius/2, hexRadius,0};
+		GeneralPath hexagon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, xPoints.length);
+		hexagon.moveTo(xPoints[0], yPoints[0]);
+		for (int index = 1; index < xPoints.length; index++) {
+		    hexagon.lineTo(xPoints[index], yPoints[index]);
+		};
+		hexagon.closePath();
 
 		// Adds Rectangles into ArrayList
-        // The rectangles represent the area inside the hexagon
-        // that contains the number
+		// The rectangles represent the area inside the hexagon
+		// that contains the number
 		squares = new ArrayList<Rectangle>();
 		count = 0;
 		for (int i = 0; i < totalRows; i++) { // i = rows
 		    for (int j = 0; j <= i; j++) { // j = index within row
 			squares.add(new Rectangle(centerX - hexRadius*2/2 + j*hexRadius*2 - i*hexRadius*2/2, 
-                centerY + i*hexRadius*3/2, hexRadius*2, hexRadius));
+						  centerY + i*hexRadius*3/2, hexRadius*2, hexRadius));
 		        count++; 
 		    }
 		}
 
 		// Draws the hexagons and numbers and fills them in appropriately
 		for (int i = 0; i < count; i++) {
-            AffineTransform transform = new AffineTransform();
-            transform.translate(squares.get(i).getX(),squares.get(i).getY());
-            Shape movedHex = transform.createTransformedShape(hexagon);
-            g2.setColor(Color.BLACK); 
-            g2.setStroke(new BasicStroke((int)(Math.sqrt(hexRadius))));
-            g2.draw(movedHex);
+		    AffineTransform transform = new AffineTransform();
+		    transform.translate(squares.get(i).getX(),squares.get(i).getY());
+		    Shape movedHex = transform.createTransformedShape(hexagon);
+		    g2.setColor(Color.BLACK); 
+		    g2.setStroke(new BasicStroke((int)(Math.sqrt(hexRadius))));
+		    g2.draw(movedHex);
 
-            g2.setColor(new Color(9,70,9));
+		    g2.setColor(new Color(9,70,9));
 		     
 		    if (numbersInPascalTriangle[i] % 2 == 1){
-                g2.setColor(new Color(129,201,130));
-                g2.fill(movedHex);
-                g2.setColor(new Color(2,63,2)); 
-            }
+			g2.setColor(new Color(129,201,130));
+			g2.fill(movedHex);
+			g2.setColor(new Color(2,63,2)); 
+		    }
 		    
 		    String num = ""+numbersInPascalTriangle[i];
 		    //make font scale with number length
 		    g2.setFont(new Font("default", Font.BOLD, (int)(hexRadius*1.5*(1-.8*num.length()/6) ) ));
 
-            //center the number
-            FontMetrics metrics = g2.getFontMetrics();
-            Rectangle2D r = metrics.getStringBounds(num, g2);
-            g2.drawString(num, (int)(squares.get(i).getX() + squares.get(i).getWidth()/2 -r.getWidth()/2),
-            (int)squares.get(i).getY()+metrics.getAscent() + (int)(squares.get(i).getHeight() - r.getHeight())/2);
+		    //center the number
+		    FontMetrics metrics = g2.getFontMetrics();
+		    Rectangle2D r = metrics.getStringBounds(num, g2);
+		    g2.drawString(num, (int)(squares.get(i).getX() + squares.get(i).getWidth()/2 -r.getWidth()/2),
+				  (int)squares.get(i).getY()+metrics.getAscent() + (int)(squares.get(i).getHeight() - r.getHeight())/2);
 		}
 
     } // End Paint Method
--- src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleGUI.java
+++ src/edu/ucsb/cs56/projects/discretemath/towers_sierpinski/PascalTriangle/PascalTriangleGUI.java
@@ -1,38 +1,82 @@
 package edu.ucsb.cs56.projects.discretemath.towers_sierpinski.PascalTriangle;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.*;
 
 /**
  *  Runs a GUI which shows Pascal's Triangle and it's relations with Sierpinski's Triangle and the Tower of Hanoi.
  *  @author George Shih
- *	@author	Caitlin Scarberry
+ *  @author Caitlin Scarberry
+ *  @author Sierra Schwellenbach
  *  @version CS56 Winter 2016
  */
 
-public class PascalTriangleGUI extends JFrame {
+public class PascalTriangleGUI extends JFrame implements ActionListener {
 
+    JButton button;
+    JTextField field;
+    PascalTriangleComponent component;
+    JFrame frame;
+        
     public static void main(String[] args) {
-    	int rows = 8;
-    	if(args.length>0)
-    		try{
-    			rows = Integer.parseInt(args[0]);
-    			if(rows<=0)
-    				throw new NumberFormatException();
-    		}
-    		catch(NumberFormatException e){
-    			System.err.println("Please input a valid number of rows (an integer >= 1)");
-    			System.exit(0);
-    		}
+	int rows = 8;
+	if(args.length>0)
+	    try{
+		rows = Integer.parseInt(args[0]);
+		if(rows<=0)
+		    throw new NumberFormatException();
+	    }
+	    catch(NumberFormatException e){
+		System.err.println("Please input a valid number of rows (an integer >= 1)");
+		System.exit(0);
+	    }
 
-		JFrame frame = new JFrame();
-		frame.setSize(800,600);
-		frame.setTitle("Pascal's Triangle GUI");
-		PascalTriangleComponent component = new PascalTriangleComponent(rows);
-		frame.add(component);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+	PascalTriangleGUI gui = new PascalTriangleGUI();
+	gui.go(rows);
     }
+    
+    public void go(int rows){
+
+	frame = new JFrame();
+		
+	frame.setSize(800,600);
+	frame.setTitle("Pascal's Triangle GUI");
+	field = new JTextField();
+	field.setBounds(5, 10, 50, 20);
+	frame.add(field);	
+	button = new JButton("Redo with new row amount");
+	button.setBounds(55, 10, 230, 20);
+	button.addActionListener(this);
+	frame.add(button);
+	component = new PascalTriangleComponent(rows);
+	frame.add(component);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event){
+	String input = field.getText();
+	int numRows = 0;
+	if(input.equals(""))
+	    return;
+	try{
+	    numRows = Integer.parseInt(input);
+	}
+	catch(Exception e){
+	    //don't do anything
+	}
+	frame.getContentPane().remove(component);
+	component = new PascalTriangleComponent(numRows);
+	frame.add(component);
+	frame.getContentPane().invalidate();
+	frame.getContentPane().validate();
+	
+    }
+
 }
+
 
 
 
